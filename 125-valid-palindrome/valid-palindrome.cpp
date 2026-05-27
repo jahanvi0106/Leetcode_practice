f69bdc65bc9f15c21d01ebfd2702c9f72527b057
Jahanvi Gajera <57354105+jahanvi0106@users.noreply.github.com>
2026-05-27 16:50:15 +0530

    Time: 3 ms (29.97%) | Memory: 10.6 MB (7.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (auto& x : s) { 
-            x = tolower(x); 
+        int n = s.length();
+        for (auto& x : s) {
+            x = tolower(x);
         }
-        string temp="";
-        for(int i=0; s[i]!='\0'; i++){
+        if(n<=1)
+            return true;
+        vector<char> v;
+        // cout<<n<<endl;
+        for(int i=0; i<n; i++){
             if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))
-                temp+=s[i];
+                v.push_back(s[i]);
         }
-        cout<<temp;
-        int n = temp.length();
-        for(int i=0; i<n/2; i++){
-            if(temp[i]!=temp[n-1-i])
+        // cout<<endl;
+        n=v.size();
+
+        if(n==0)
+            return true;
+        for(int i=0; i<=n/2; i++){
+            // cout<<v[i];
+            if(v[i]!=v[n-i-1])
                 return false;
         }
         return true;
