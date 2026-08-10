5c87fe10b263aff1106a6b18d096857e8563ece2
Jahanvi Gajera <57354105+jahanvi0106@users.noreply.github.com>
2026-08-10 14:36:01 +0530

    Time: 19 ms (13.66%) | Memory: 19 MB (70.39%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -19,28 +19,60 @@ public:
 class Solution {
 public:
 
-    Node* connect(Node* root) {
-        if (!root) return root;
-        queue<Node*> q;
-        q.push(root);
+    Node *getNextRight(Node *p) {
 
-        while (!q.empty()) {
-            int nodeCount = q.size();
-
-            for (int i = 0; i < nodeCount; i++) {
-                Node* node = q.front();
-                q.pop();
-
-                // Set nextRight
-                if (i == nodeCount - 1)
-                    node->next = nullptr;
-                else
-                    node->next = q.front();
-                // Push children
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+        Node *temp = p->next;
+    
+        // Keep moving to next sibling until
+        // you find a sibling that has at least
+        // one children
+        while (temp != nullptr) {
+            if (temp->left != nullptr)
+                return temp->left;
+            if (temp->right != nullptr)
+                return temp->right;
+            temp = temp->next;
+        }
+    
+        return nullptr;
+    }
+    
+    // Function to connect nodes at the same level
+    Node* connect(Node *root) {
+    
+        Node *temp = root;
+        if (!temp)
+            return nullptr ;
+    
+        temp->next = nullptr;
+        
+        while (temp != nullptr) {
+            Node *q = temp;
+    
+            // Traverse all nodes at the current level
+            while (q != nullptr) {
+                if (q->left) {
+    
+                    if (q->right)
+                        q->left->next = q->right;
+                    else
+                        q->left->next = getNextRight(q);
+                }
+    
+                if (q->right)
+                    q->right->next = getNextRight(q);
+                q = q->next;
             }
+    
+            // Move to the next level starting with the leftmost node
+            if (temp->left)
+                temp = temp->left;
+            else if (temp->right)
+                temp = temp->right;
+            else
+                temp = getNextRight(temp);
         }
         return root;
     }
+
 };
